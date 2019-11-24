5b52ce8295ecd09d1282cea3b8544eb86edae613
Claire <smith.clairelouise@gmail.com>
2019-11-24 19:04:03 +0000

    url paths updated

--- frontend/urls.py
+++ frontend/urls.py
@@ -4,15 +4,15 @@ from .views import Home, Assets
 
 # new version
 
-# urlpatterns = [
-#     # path('', Home.as_view()),
-#     re_path(r'^[\w\?\=\-\/]*$', Home.as_view()),
-#     re_path(r'(?P<filename>(assets/)?[\w\.\-\/]+)$', Assets.as_view())
-# ]
+urlpatterns = [
+    # path('', Home.as_view()),
+    re_path(r'^[\w\?\=\-\/]*$', Home.as_view()),
+    re_path(r'(?P<filename>(assets/)?[\w\.\-\/]+)$', Assets.as_view())
+]
 
 # old version
 
-urlpatterns = [
-    path('', Home.as_view()),
-    re_path(r'^(?P<filename>[\w\.]+)$', Assets.as_view())
-]
+# urlpatterns = [
+#     path('', Home.as_view()),
+#     re_path(r'^(?P<filename>[\w\.]+)$', Assets.as_view())
+# ]
